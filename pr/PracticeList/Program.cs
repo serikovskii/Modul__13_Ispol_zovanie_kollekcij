5e033be0d34fc3693ab67099a68b854c1145dbaa
Azat <46015824+serikovskii@users.noreply.github.com>
2019-03-26 19:06:20 +0600

    practica done

---
+++ pr/PracticeList/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> arrayList = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+            
+            List<int> tmpArr = new List<int>();
+            foreach (int value in arrayList)
+            {
+                if (value % 2==0)
+                {
+                    tmpArr.Add(value);
+                }
+            }
+            
+            arrayList.Clear();
+            
+            arrayList = tmpArr;
+            
+            foreach (int value in arrayList)
+            {
+                Console.WriteLine(value);
+            }
+            Console.WriteLine("2 задание");
+            foreach (int value in arrayList)
+            {
+                if(value > arrayList.Average())
+                    Console.WriteLine(value);
+            }
+
+
+        }
+    }
+}
